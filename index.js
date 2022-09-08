43e8a67cda39f613135c773ecead8134c09fde4e
Brooke Ross <brooke.ross2800@gmail.com>
2022-09-07 19:14:17 -0600

    commit tree aab0a5e54c1d3d83ec1ae6c00e7ec22c1f34e31d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const doger = document.getElementById("dodger");
+dodger.style.backgroundColor="#FF69B4";
+dodger.style.bottom="0px";
+dodger.style.left="0px";
+
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left >= 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+  
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+  })
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
